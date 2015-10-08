b61a9e220ee9498bc59fda54fdf8de0870bc139c
Erik Öjebo <erik.ojebo@gmail.com>
2015-10-08 12:03:23 +0200

    Made bot class name case insensitive

--- src/aspnet/BotArena/Scripts/game/modifiers/classes/classFactory.js
+++ src/aspnet/BotArena/Scripts/game/modifiers/classes/classFactory.js
@@ -4,11 +4,12 @@ gosuArena.factories.modifiers = gosuArena.factories.modifiers || {};
 
 gosuArena.factories.modifiers.createClassFromOptions = function (botClassName) {
 
-    // to lower
+    var safeClassName = botClassName || "default";
+    var lowercaseClassName = safeClassName.toLowerCase();
     
     var defaultClass = gosuArena.factories.modifiers.classes.default.create();
 
-    switch (botClassName) {
+    switch (lowercaseClassName) {
         case "tank":
             return gosuArena.factories.modifiers.classes.tank.create(defaultClass);
         default:
