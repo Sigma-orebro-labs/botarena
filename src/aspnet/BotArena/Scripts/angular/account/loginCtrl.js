0209ebcd4281f481b7372da1ebebff542e4eb751
Erik Öjebo <erik.ojebo@gmail.com>
2015-12-30 15:46:12 +0100

    Added some minor fixes to the login screen in the menu

--- src/aspnet/BotArena/Scripts/angular/account/loginCtrl.js
+++ src/aspnet/BotArena/Scripts/angular/account/loginCtrl.js
@@ -1,4 +1,4 @@
-﻿angular.module('menuApp').controller('loginCtrl', ['$scope','$http', function ($scope, $http) {
+﻿angular.module('menuApp').controller('loginCtrl', ['$scope','$http', '$state', function ($scope, $http, $state) {
 
             $scope.model = {
                 UserName: "",
@@ -6,21 +6,30 @@
                 RememberMe: false
             };
 
-            $scope.login = function() {
+            $scope.isProcessingLoginRequest = false;
+            $scope.hasLoginFailed = false;
+
+            $scope.login = function () {
+
+                $scope.isProcessingLoginRequest = true;
+
                 console.log("loginMethod");
                 $http({
-                    method: 'POST',
-                    url: gosuArena.url.createAbsolute('/api/AuthSession/'),
+                    method: "POST",
+                    url: gosuArena.url.createAbsolute("/api/AuthSession/"),
                     data: $scope.model
-                }).then(function () {
+                }).then(function() {
+                    $state.go("none");
                     console.log("login was successful");
-                }, function (e) {
 
+                    $scope.isProcessingLoginRequest = false;
+                }, function(e) {
                     if (e.status === 401) {
                         alert("Username and/or password was incorrect");
                     }
 
-                    console.log(JSON.stringify(e));
+                    $scope.hasLoginFailed = true;
+                    $scope.isProcessingLoginRequest = false;
                 });
             };
         }
