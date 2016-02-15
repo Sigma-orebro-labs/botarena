c252fc7b6f38c0015bf62cbdce2cfb3db546db30
Erik Öjebo <erik.ojebo@gmail.com>
2016-02-15 22:55:25 +0100

    updated docs

--- src/aspnet/BotArena/Scripts/bots/bootstrapping/behaviour/circler.js
+++ src/aspnet/BotArena/Scripts/bots/bootstrapping/behaviour/circler.js
@@ -1,32 +1,6 @@
-﻿
-        actionQueue.clear();
-
-        if (status.canFire()) {
+﻿        if (status.canFire()) {
             actionQueue.fire();
         }
 
         actionQueue.turn(1);
         actionQueue.forward(2);
-    },
-    onHitByBullet: function (actionQueue, status, augmentations, eventArgs) {
-
-        if (!augmentations.cloak.isActive() && augmentations.cloak.roundsRemaining() > 0) {
-            augmentations.cloak.activate();
-        }
-
-        actionQueue.clear();
-
-        if (status.canMoveLeft()) {
-            actionQueue.left(10);            
-        } else {
-            actionQueue.right(10);
-        }
-
-        var degreesToTurn = eventArgs.angle - status.angle;
-
-        actionQueue.turn(degreesToTurn);
-    },
-    options: {
-        augmentations: ["cloak"]
-    }
-});
